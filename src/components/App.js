39214c3b02af73e586dc894f03b46436850a8dbb
Arjun Singh <arjunsinghbazal@gmail.com>
2023-06-14 15:02:27 +0530

    submitting code-Wed Jun 14 2023 15:02:26 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, {Component, useState} from "react";
+import '../styles/App.css';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div  id="main">
+      <p>I am learning React. My life is getting better.</p>
     </div>
   )
 }
 
-export default App
+
+export default App;
